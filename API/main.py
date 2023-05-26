5d7c2a1c280a1ffb781dc7135c3e56e63a3ed055
kevin-krause <contatokevinkrause@gmail.com>
2023-05-26 12:58:12 -0300

    -

--- API/main.py
+++ API/main.py
@@ -1,32 +1,45 @@
-
-"""
-1. objetivo
-2. url base
-3. endpoints
-    localhot/meds (get)
-    localhost/meds (post)
-    localhost/meds (delete)
-    localhost/meds/id (get)
-    localhost/meds/id (put)
-
-4. recursos
-"""
-
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-meds = [{
-    "id": 1,
-    "identificacao": [{"medicamento": "Acetilcisteína",
-                      "classe": "mucolítico e expectorante",
-                       "apresentacao": "Amp. 3 mL (100 mg/mL)",
-                       "reconstituicao": "Já vem em solução"}],
-    "vias de admistração": [{
-        "inalatória": "sim",
-        "itramuscular": "não",
-        "endovenosa_ou_direto": "prevenção nefropatia porcontraste: 5 – 10 min",
-        "infusao": "intoxicação por paracetamol Dose 1: 1 hora Dose 2: 4 horas Dose 3: 16 horas"
-    }]
+meds = [
+    {
+        "id": 1,
+        "identificacao": [
+            {
+                "medicamento": "Acetilcisteína",
+                "classe": "mucolítico e expectorante",
+                "apresentacao": "Amp. 3 mL (100 mg/mL)",
+                "reconstituicao": "Já vem em solução",
+            }
+        ],
+        "vias de admistração": [
+            {
+                "inalatória": "sim",
+                "itramuscular": "não",
+                "endovenosa_ou_direto": "prevenção nefropatia porcontraste: 5 – 10 min",
+                "infusao": "intoxicação por paracetamol Dose 1: 1 hora Dose 2: 4 horas Dose 3: 16 horas",
+            }
+        ],
+        "soluções p/ diluição": [
+            {"SF": "Volume de soro igual ao volume do medicamento", "Dose": "1 hora"}
+        ],
+    }
+]
 
-}]
+
+@app.route("/meds", methods=["GET"])
+def get_meds():
+    return jsonify(meds)
+
+
+@app.route("/meds/<int:med_id>", methods=["GET"])
+def get_med(med_id):
+    for med in meds:
+        if med["id"] == med_id:
+            return jsonify(med)
+    return jsonify({"error": "Medication not found"})
+
+
+if __name__ == "__main__":
+    app.run()
